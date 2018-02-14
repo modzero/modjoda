0a3227aba7d712e3b0595463169c54d8386849e5
Jan Girlich <jan.girlich@modzero.ch>
2018-02-14 09:46:19 +0100

    removing unused and unnecessary code

--- deserialization_receiver/app/src/main/java/ch/modzero/deserialization_demo/MainActivity.java
+++ deserialization_receiver/app/src/main/java/ch/modzero/deserialization_demo/MainActivity.java
@@ -1,16 +1,13 @@
 package ch.modzero.deserialization_demo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.io.FileOutputStream;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
